9bbe6c93eb3c9049904f87d6670fe5f05c209772
KM Musshafen <kmusshafen@hotmail.com>
2023-04-15 11:58:34 -0400

    Added songs

--- queries.sql
+++ queries.sql
@@ -59,3 +59,33 @@ VALUES (2, 'Yourself or Someone Like You', 'False', '1996', 2);
 INSERT INTO "Album" ("Id", "Title", "IsExplicit", "ReleaseDate", "BandId")
 VALUES (3, 'Say It Like You Mean It', 'False', '2002', 3);
 
+
+//Adding Songs
+INSERT INTO "Song" ("Id", "TrackNumber", "Title", "Duration", "AlbumId")
+VALUES (1, "2" "Omaha", "3:39", 1);
+
+INSERT INTO "Song" ("Id", "TrackNumber", "Title", "Duration", "AlbumId")
+VALUES (2, "3", "Mr. Jones", "4:33", 1 );
+
+INSERT INTO "Song" ("Id", "TrackNumber", "Title", "Duration", "AlbumId")
+VALUES (3, "5", "Anna Begins", "4:31", 1);
+
+
+INSERT INTO "Song" ("Id", "TrackNumber", "Title", "Duration", "AlbumId")
+VALUES (4, "3", "3 am", "3:47", 2);
+
+INSERT INTO "Song" ("Id", "TrackNumber", "Title", "Duration", "AlbumId")
+VALUES (5, "4", "Push", "3:59", 2);
+
+INSERT INTO "Song" ("Id", "TrackNumber", "Title", "Duration", "AlbumId")
+VALUES (6, "12", "Hang", "3:47", 2);
+
+INSERT INTO "Song" ("Id", "TrackNumber", "Title", "Duration", "AlbumId")
+VALUES (7, "4", "The Best of Me", "4:18", 3);
+
+INSERT INTO "Song" ("Id", "TrackNumber", "Title", "Duration", "AlbumId")
+VALUES (8, "5", "A Goodnight's Sleep", "4:23", 3);
+
+INSERT INTO "Song" ("Id", "TrackNumber", "Title", "Duration", "AlbumId")
+VALUES (9, "9", "Decisions, Decisions", "3:51", 3);
+
