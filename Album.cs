aa11cf3328e600f337f3f80023c2867047434f8e
KM Musshafen <kmusshafen@hotmail.com>
2023-04-18 19:34:40 -0400

    creating Album class

--- Album.cs
+++ Album.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+
 namespace RhythmsGonnaGetYou
 {
     public class Album
     {
-        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsExplicit { get; set; }
+        public string ReleaseDate { get; set; }
+        public int BandsId { get; set; }
+
+        public Band Band { get; set; }
+        // public List<Song> Songs { get; set;}
+
+
     }
 }
