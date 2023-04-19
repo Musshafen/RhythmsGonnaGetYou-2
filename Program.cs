ab1d1d99d2dfb2bbf3a9187cca60fb55a6cdd695
KM Musshafen <kmusshafen@hotmail.com>
2023-04-18 20:22:42 -0400

    created display menu working code

--- Program.cs
+++ Program.cs
@@ -5,13 +5,34 @@ namespace RhythmsGonnaGetYou
 {
     class Program
     {
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("[A]dd a new band, album, or song");
+            Console.WriteLine("[V]iew band and album info");
+            Console.WriteLine("[U]pdate status of a band");
+            Console.WriteLine("[Q]uit");
+            Console.WriteLine();
+
+        }
+
+
         static void Main(string[] args)
         {
             var context = new RhythmsGonnaGetYouContext();
 
+            DisplayMenu();
+
             var bandCount = context.Bands.Count();
 
             Console.WriteLine($"There are {bandCount} bands!");
+
+
+
+
+
         }
     }
 }
