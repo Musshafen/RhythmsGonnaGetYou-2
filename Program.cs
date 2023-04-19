2d9244d26288311157613e336532e691df874a68
KM Musshafen <kmusshafen@hotmail.com>
2023-04-18 20:27:10 -0400

    Added prompt for INT

--- Program.cs
+++ Program.cs
@@ -24,6 +24,22 @@ namespace RhythmsGonnaGetYou
 
             return userInput;
         }
+         public static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer. ");
+                return 0;
+            }
+        }
 
 
         static void Main(string[] args)
