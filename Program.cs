5109db93865e06367c8447591a52481403eca4cc
KM Musshafen <kmusshafen@hotmail.com>
2023-04-21 21:15:02 -0400

    context class and menu options

--- Program.cs
+++ Program.cs
@@ -1,22 +1,69 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace RhythmsGonnaGetYou
 {
 
- 
+    class RhythmsGonnaGetYouContext : DbContext
+    {
+        public DbSet<Band> Bands { get; set; }
+         public DbSet<Album> Albums { get; set; }
+         public DbSet<Song> Songs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            // optionsBuilder.UseLoggerFactory(loggerFactory);
+
+            optionsBuilder.UseNpgsql("server=localhost;database=RhythmsGonnaGetYou");
+        }
+    }
+
+     class MenuOptions
+    {
+        public void AddAlbum(string albumTitle, int bandId, string isExplicit)
+        {
+            var context = new RhythmsGonnaGetYouContext();
+            var album = new Album
+            {
+                Title = albumTitle,
+                ReleaseDate = DateTime.Now,
+                BandId = bandId,
+                IsExplicit = isExplicit
+            };
+            context.Albums.Add(album);
+            context.SaveChanges();
+        }
+
+        public void AddSong(int trackNumber, string songTitle, decimal duration, int albumId)
+        {
+            var context = new RhythmsGonnaGetYouContext();
+            var song = new Song
+            {
+                TrackNumber = trackNumber,
+                Title = songTitle,
+                Duration = duration,
+                AlbumId = albumId
+
+            };
+            context.Songs.Add(song);
+            context.SaveChanges();
+        }
+    }
+
     class Program
     {
 
         static void Main(string[] args)
         {
-         
-   
 
 
-                Console.WriteLine("Welcome");
 
-          
+
+            Console.WriteLine("Welcome");
+
+
 
 
 
